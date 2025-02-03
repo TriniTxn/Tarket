7a1b1dc3d6154960f225155367ca80b83940d34f
TriniTxn <giovanni.sgomes@gmail.com>
2025-02-03 16:42:31 -0300

    First configs (Prod & Test)

---
+++ src/main/java/br/com/devannis/webmarket/WebmarketApplication.java
@@ -0,0 +1,13 @@
+package br.com.devannis.webmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebmarketApplication.class, args);
+	}
+
+}
