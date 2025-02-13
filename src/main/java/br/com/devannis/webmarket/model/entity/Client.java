4218ee9b390a42ad4c645dabd579f9355936c4b0
TriniTxn <giovanni.sgomes@gmail.com>
2025-02-13 00:47:23 -0300

    Adding Client Entity

---
+++ src/main/java/br/com/devannis/webmarket/model/entity/Client.java
@@ -0,0 +1,29 @@
+package br.com.devannis.webmarket.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "TB_CLIENTS")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_CLIENTS")
+    @SequenceGenerator(name = "SEQ_CLIENTS", sequenceName = "SEQ_CLIENTS", allocationSize = 1)
+    @Column(name = "client_id")
+    private Long clientId;
+
+    @Column(name = "client_name")
+    private String clientName;
+
+    @Column(name = "client_address")
+    private String clientAddress;
+
+    @Column(name = "id_number")
+    private String idNumber;
+}
